b96e0a88e5c1e62e847fbf600ebd7dc64a6ce539
Alex Mekelburg <alex@mekware.com>
2019-10-21 09:27:27 -0400

    update cookie consent display

--- _includes/reztrip/setup-cookie-consent.js
+++ _includes/reztrip/setup-cookie-consent.js
@@ -2,11 +2,11 @@ window.cookieConsentConfig = {
   timeout: 5000,
   location: false,
   content: {
-    message: "This site uses cookies for best experience. By continuing you consent to our <a href=\"#\">Cookie Policy.</a>",
+    message: "This website uses cookies for best experience. By continuing you consent to our <a href=\"#\">Cookie Policy.</a>",
     dismiss: 'X',
   },
   elements: {
-    dismiss: '<a title="dismiss cookie message" tabindex="0" style="display:block; cursor: pointer; font-size: 20px; background: none; width: auto; min-width: 0;" class="cc-btn cc-dismiss">{{dismiss}}</a>',
+    dismiss: '<a title="dismiss cookie message" tabindex="0" style="display:block; cursor: pointer; font-size: 20px; background: none; width: auto; min-width: 0;" class="cc-btn cc-dismiss">X</a>',
   },
   showLink: false,
   palette: {
